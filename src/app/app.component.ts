f8886c8d0a43f95df3ba9626b341f77d998da261
Goury Shanker <gourykr@gmail.com>
2019-08-20 01:19:06 +0530

    befor build creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { Nav,Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+
+//import { TabsPage } from '../pages/tabs/tabs';
+//import { LoginPage } from '../pages/login/login';
+import { MainPage } from '../pages/main/main';
+//import { MarriagecoordinatorsPage } from '../pages/marriagecoordinators/marriagecoordinators';
+//import { UserlistPage } from '../pages/userlist/userlist';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+@ViewChild(Nav) nav: Nav;
+  rootPage:any = MainPage;
+  pages: Array<{title: string, component: any, icon: string}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
+
+   // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'User List', component: 'UserlistPage', icon: 'home' },
+      { title: 'Marriage Coordinators', component: 'MarriagecoordinatorsPage', icon: 'contacts' }
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +33,17 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+   openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.nav.setRoot('LoginPage');
+  }
+
+  
 }
---
+++ src/pages/ftabs/ftabs.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+
+/**
+ * Generated class for the FtabsPage tabs.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-ftabs',
+  templateUrl: 'ftabs.html'
+})
+export class FtabsPage {
+
+  homeRoot = 'FhomePage'
+  profileRoot = 'ProfilePage'
+  walletsRoot = 'WalletsPage'
+  referRoot = 'ReferPage'
+
+
+  constructor(public navCtrl: NavController) {}
+
+}
